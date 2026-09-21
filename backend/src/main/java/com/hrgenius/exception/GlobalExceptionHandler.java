224cae0e21fd4b81588b420914cf86346c5381e5
Kunal Kumar <4002kunal88@gmail.com>
2026-09-21 23:52:24 +0530

    fixed small bugs and updated routes

---
+++ backend/src/main/java/com/hrgenius/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.hrgenius.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntime(RuntimeException ex) {
+        return ResponseEntity.badRequest().body(Map.of("error", ex.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGeneral(Exception ex) {
+        return ResponseEntity.internalServerError().body(Map.of("error", "Something went wrong"));
+    }
+}
